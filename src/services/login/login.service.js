85a970b01a4ea904ed9521c1698449f4336da787
Allex-Lima <alexlmangabeira@gmail.com>
2024-02-20 16:02:02 -0300

    Feat: criando token (JWT)

--- src/services/login/login.service.js
+++ src/services/login/login.service.js
@@ -1,6 +1,9 @@
 import bcrypt from "bcrypt";
 import { findUserByEmail } from "../../repositories/user/user.Repository.js";
 import { validationLogin } from "../../validate/validate.js";
+import jwt from "jsonwebtoken";
+
+import "dotenv/config";
 
 
 export const loginService = async (dataUser) => {
@@ -16,11 +19,15 @@ export const loginService = async (dataUser) => {
 			return [];
 		}
 
-		const passwordIsValid = await bcrypt.compare(dataUser.password, user.password);
-		
-		return passwordIsValid;
+		await bcrypt.compare(dataUser.password, user.password);
+
+		const token = generateToken(user.id);
+
+		return token;
         
 	} catch (error) {
 		return { message: error.message };
 	}
 };
+
+export const generateToken = (id) => jwt.sign({id: id}, process.env.SECRET_JWT, {expiresIn: 86400});
